e7c6eb0e9fbd0c4b24e3e290fb147f22b7cc89a2
adurodo1 <adurodo1@my.centennialcollege.ca>
2021-07-13 23:33:38 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {title: 'Landing Page'});
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', {title: 'Landing Page'});
+});
+
+router.get('/survey', function(req, res, next){
+  res.render('survey', {title: 'Create Survey Page'})
+})
+
+router.get('/surveyDB', function(req, res, next){
+  res.render('surveyDB', {title: 'Existing Survey Page'})
+})
+
+
+module.exports = router;
