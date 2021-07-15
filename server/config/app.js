2161481b1fb0827fc428b99468d4b993e8d1f689
adurodo1 <adurodo1@my.centennialcollege.ca>
2021-07-15 09:25:23 -0400

    sixth commit

--- server/config/app.js
+++ server/config/app.js
@@ -5,6 +5,20 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+// import "mongoose" - required for DB Access
+let mongoose = require('mongoose');
+// URI
+let DB = require('./db');
+
+mongoose.connect(process.env.URI || DB.URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+mongoDB.once('open', ()=> {
+  console.log("Connected to MongoDB...");
+});
+
+//Router Definition
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
 
---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //local MongoDB deployment ->
+    "URI": "mongodb://localhost/books"
+   // "URI":"mongodb+srv://Saheed22:Saheed22@cluster0.lirry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  };
+  
